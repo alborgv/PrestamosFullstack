630784789d189b730b48c8690b4c0c7530ff7d02
Abel Albor <abelalbor004@gmail.comcls>
2024-08-14 09:26:51 -0500

    settings update

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.static.StaticFilesMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
