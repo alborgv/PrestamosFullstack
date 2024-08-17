47d13d4d980f3a50fea0e3f2a2e18a29f7df19db
Abel Albor <abelalbor004@gmail.com>
2024-08-17 13:09:15 -0500

    update settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -200,7 +200,4 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000"
-]
+CORS_ALLOWED_ORIGINS = ["*"]
