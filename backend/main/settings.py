ef891ebd793a12eda03dbf110aef99c87c1b40e5
Abel Albor <abelalbor004@gmail.com>
2024-08-19 00:32:51 -0500

    update2

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -103,20 +103,11 @@ DATABASES = {
         'ENGINE': 'mysql.connector.django',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),  # Define DATABASE_PASSWORD in your .env file
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
     }
 }
-
-
-# DATABASES = {
-    
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
